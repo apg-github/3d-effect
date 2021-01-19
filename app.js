90cd6f0cb6a7e7ece2e63871a49ccd62c3e37a2f
APG <apg.prywatny@gmail.com>
2021-01-19 22:05:53 +0100

    3d effect working

---
+++ app.js
@@ -0,0 +1,34 @@
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+
+const title = document.querySelector(".title");
+const volvo = document.querySelector(".volvo img");
+const purchase = document.querySelector(".purchase button");
+const description = document.querySelector(".info h3");
+const engines = document.querySelector(".engines");
+
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 22;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 22;
+
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+  title.style.transform = "translateZ(150px)";
+  volvo.style.transform = "translateZ(100px) rotateZ(-5deg)";
+  purchase.style.transform = "translateZ(150px)";
+  description.style.transform = "translateZ(75px)";
+  engines.style.transform = "translateZ(75px)";
+});
+
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = "rotateY(0deg) rotateX(0deg)";
+  title.style.transform = "translateZ(0px)";
+  volvo.style.transform = "translateZ(0px) rotateZ(0deg)";
+  purchase.style.transform = "translateZ(0px)";
+  description.style.transform = "translateZ(0px)";
+  engines.style.transform = "translateZ(0px)";
+});
